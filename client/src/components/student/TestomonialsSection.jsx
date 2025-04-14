29842739cb79cc53146cb431fd013d4656463739
jana tech <janatalari2020@gmail.com>
2025-04-14 11:54:57 +0530

    Initial commit for LearnFlow LMS

---
+++ client/src/components/student/TestomonialsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestomonialsSection = () => {
+  return (
+    <div>
+        <h1>TestomonialsSection</h1>
+    </div>
+  )
+}
+
+export default TestomonialsSection
