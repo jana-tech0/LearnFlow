82761ddfb716645dc85f2aa595a16a08b5bd9a60
jana tech <janatalari2020@gmail.com>
2025-04-16 18:34:21 +0530

    create courseSecion and card container

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import CourseCard from './CourseCard'
 
 const CoursesSection = () => {
+
+    const {allCourses} = useContext(AppContext)
+
   return (
     <div className='py-16 md:px-40 px-8'>
 
         <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
         <p className='text-sm md:text-base text-gray-500 mt-3'>Learn from industry experts, master in-demand skills, and accelerate your learning journey with our most loved and highest-rated courses on LearnFlow.</p>
+        
+        <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] px-4 md:px-0 md:my-16 my-10 gap-4'>
+            {allCourses.slice(0,4).map((course,index) => <CourseCard key={index} course = {course} />)}
+        </div>
+
         <Link to={'/course-list'} onClick={() => scrollTo(0,0)} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'> Show All Courses</Link>
     </div>
   )
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,32 @@
+import React, { useState, useEffect, useContext } from 'react';
 import { createContext } from "react";
-
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from 'react-router-dom';
 export const AppContext = createContext();
 
+
 export const AppContextProvider = (props) => {
-   
+     
+    const currency = import.meta.env.VITE_CURRENCY
+     const navigate = useNavigate()
+    const [allCourses, setAllCourses] = useState([])
+
+    // Fetch all courses 
+
+    const fetchAllCourses = async() => {
+        setAllCourses(dummyCourses)
+    }
 
     const value = {
-        
+        currency,
+        allCourses,
+        navigate
     }
+
+    useEffect(() => {
+          fetchAllCourses()
+    },[])
+
 
   return(
     <AppContext.Provider value={value}>
