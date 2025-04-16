0f0087bc2b0b568f7be8ebe790b73730893473be
jana tech <janatalari2020@gmail.com>
2025-04-16 14:33:01 +0530

    course-secion details

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CoursesSection = () => {
   return (
-    <div>
-        <h1>Courses section</h1>
+    <div className='py-16 md:px-40 px-8'>
+
+        <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+        <p className='text-sm md:text-base text-gray-500 mt-3'>Learn from industry experts, master in-demand skills, and accelerate your learning journey with our most loved and highest-rated courses on LearnFlow.</p>
+        <Link to={'/course-list'} onClick={() => scrollTo(0,0)} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'> Show All Courses</Link>
     </div>
   )
 }
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/Companies'
+import CoursesSection from '../../components/student/CoursesSection'
+import CourseCard from '../../components/student/CourseCard'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
         <Hero />
+        <Companies />
+        <CoursesSection/>
+        
     </div>
   )
 }
